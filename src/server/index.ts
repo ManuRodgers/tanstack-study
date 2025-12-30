ca83d24e9775b006c536463a4706d5a04d9d47d8
Manu <manurodgersgoat@gmail.com>
2025-12-30 14:31:51 +0800

    remove index.ts

--- src/server/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Server module exports
- * This file serves as the main entry point for server-related exports
- */
-
-export { type App, app } from "./app";
-export { names } from "./routes/names";
-export { todos } from "./routes/todos";
